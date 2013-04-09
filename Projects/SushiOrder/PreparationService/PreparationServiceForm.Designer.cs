2d30cadfda09c42cc30c963451acad3247a53c3f
DavidRego <Stravus.Editions@gmail.com>
2013-04-09 12:15:47 +0100

    QQcoisa 3

--- Projects/SushiOrder/PreparationService/PreparationServiceForm.Designer.cs
+++ Projects/SushiOrder/PreparationService/PreparationServiceForm.Designer.cs
@@ -52,6 +52,7 @@
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(223, 316);
             this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedValueChanged += new System.EventHandler(this.listBox2_SelectedValueChanged);
             // 
             // label1
             // 
