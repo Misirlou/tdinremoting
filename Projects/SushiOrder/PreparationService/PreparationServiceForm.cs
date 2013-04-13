68eeb6417b1eedba43beb2a0e8d6fd60eebc9248
Misirlou <diogotbasto@gmail.com>
2013-04-13 13:47:10 +0100

    preparation form a 100%

--- Projects/SushiOrder/PreparationService/PreparationServiceForm.cs
+++ Projects/SushiOrder/PreparationService/PreparationServiceForm.cs
@@ -22,7 +22,7 @@ namespace PreparationService
         {
             InitializeComponent();
 
-            
+
             RemotingConfiguration.Configure("PreparationService.exe.config", false);
             lorders = (IOrders)RemoteNew.New(typeof(IOrders));
             evproxy = new AlterEventProxy();
@@ -31,19 +31,40 @@ namespace PreparationService
 
             this.label1.Text = "Novas Encomendas";
             this.label2.Text = "Em Preparação";
-            
+            this.button1.Text = "Por em preparação";
+
         }
 
-        public void doAlterations(OrderState state , Order or)
+        public void doAlterations(OrderState state, Order or)
         {
-            if (state == OrderState.New)
+            switch (state)
             {
+                case OrderState.New: UpdateNew(); break;
+                case OrderState.Preparing: UpdatePreparing(); break;
+                case OrderState.Ready: UpdateReady(); break;
+            }
 
+        }
+
+
+        public void UpdateNew()
+        {
+            if (InvokeRequired)
+                Invoke(new MethodInvoker(UpdateNew));
+            else
+            {
                 this.listBox1.DataSource = lorders.GetOrdersByState(OrderState.New);
                 this.listBox1.DisplayMember = "DisplayMember";
                 this.listBox1.ValueMember = "Nr";
             }
-            else if(state == OrderState.Preparing)
+        }
+
+        public void UpdatePreparing()
+        {
+
+            if (InvokeRequired)
+                Invoke(new MethodInvoker(UpdatePreparing));
+            else
             {
                 this.listBox1.DataSource = lorders.GetOrdersByState(OrderState.New);
                 this.listBox1.DisplayMember = "DisplayMember";
@@ -53,46 +74,65 @@ namespace PreparationService
                 this.listBox2.DisplayMember = "DisplayMember";
                 this.listBox2.ValueMember = "Nr";
             }
-            else if (state == OrderState.Ready)
+        }
+
+        public void UpdateReady()
+        {
+            if (InvokeRequired)
+                Invoke(new MethodInvoker(UpdateReady));
+            else
             {
                 this.listBox2.DataSource = lorders.GetOrdersByState(OrderState.Preparing);
                 this.listBox2.DisplayMember = "DisplayMember";
                 this.listBox2.ValueMember = "Nr";
             }
-
-        }
-
-
-        public void getNewOrders()
-        {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(this.button1.Text.Equals("Por em preparação")){
-                int num = (int)this.listBox1.SelectedValue;
-              
-                lorders.ModifyOrder( num, OrderState.Preparing);
+            if (this.button1.Text.Equals("Por em preparação"))
+            {
+                if (this.listBox1.SelectedValue != null)
+                {
+                    int num = (int)this.listBox1.SelectedValue;
+                    lorders.ModifyOrder(num, OrderState.Preparing);
+                }
             }
             else if (this.button1.Text.Equals("Enviar para entrega"))
             {
-
-                int num = (int)this.listBox1.SelectedValue;
-                lorders.ModifyOrder(num, OrderState.Ready);
+                if (this.listBox2.SelectedValue != null)
+                {
+                    int num = (int)this.listBox2.SelectedValue;
+                    lorders.ModifyOrder(num, OrderState.Ready);
+                }
 
             }
-          
+
         }
 
         private void listBox1_SelectedValueChanged(object sender, EventArgs e)
         {
             this.button1.Text = "Por em preparação";
+            updateTextBox((Order)listBox1.SelectedItem);
+
         }
 
         private void listBox2_SelectedValueChanged(object sender, EventArgs e)
         {
             this.button1.Text = "Enviar para entrega";
+            updateTextBox((Order)listBox2.SelectedItem);
+        }
+
+        private void updateTextBox(Order o)
+        {
+
+            string str = "";
+            str += "Nr: " + o.Nr.ToString() + "\r\nNome: " + o.Name + "\r\nMorada: " + o.Address + "\r\nNumero cartao: " + o.CreditCard + "\r\nEstado: " + Enum.GetName(typeof(OrderState), o.Estado)+"\r\n";
+            foreach (OrderItem oi in o.produtos)
+            {
+                str += "\r\n" + Enum.GetName(typeof(MenuItem), oi.Type) + ": " + oi.Nr.ToString();
+            }
+            this.textBox1.Text = str;
         }
 
         public override object InitializeLifetimeService()
